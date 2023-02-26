84246361e6f0c6d196c87a2ebdab79513b85590d
hdStrn <nomovemc@gmail.com>
2023-02-26 17:28:42 +0300

    Cards initialization moved to main

--- src/main/java/ru/netology/netologydiplomamoneytransferservice/repository/CardRepositoryImpl.java
+++ src/main/java/ru/netology/netologydiplomamoneytransferservice/repository/CardRepositoryImpl.java
@@ -1,9 +1,8 @@
 package ru.netology.netologydiplomamoneytransferservice.repository;
 
-import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import org.springframework.stereotype.Repository;
 import ru.netology.netologydiplomamoneytransferservice.domain.Card;
-import ru.netology.netologydiplomamoneytransferservice.api.dto.Amount;
 
 import java.util.Map;
 import java.util.Optional;
@@ -12,35 +11,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class CardRepositoryImpl implements CardRepository {
 
+    @Getter
     private final Map<String, Card> cards = new ConcurrentHashMap<>();
 
     @Override
     public Optional<Card> getCardByNumber(String cardNumber) {
         return Optional.ofNullable(cards.get(cardNumber));
-    }
-
-    @PostConstruct
-    public void init() {
-        cards.put("1111222233334444", new Card("1111222233334444", "01/30", "111",
-                new ConcurrentHashMap<>(Map.of("RUR", new Amount(50000, "RUR")))));
-
-        cards.put("9999888877776666", new Card("9999888877776666", "10/25", "222",
-                new ConcurrentHashMap<>(Map.of("EUR", new Amount(100000, "EUR")))));
-
-        cards.put("1234567890123456", new Card("1234567890123456", "05/27", "333",
-                new ConcurrentHashMap<>(Map.of("USD", new Amount(10000, "USD")))));
-
-        cards.put("1122334455667788", new Card("1122334455667788", "03/31", "444",
-                new ConcurrentHashMap<>(Map.of(
-                        "RUR", new Amount(150000, "RUR"),
-                        "EUR", new Amount(20000, "EUR")
-                ))));
-
-        cards.put("9876543210987654", new Card("9876543210987654", "08/24", "555",
-                new ConcurrentHashMap<>(Map.of(
-                        "RUR", new Amount(250000, "RUR"),
-                        "USD", new Amount(5000, "USD"),
-                        "EUR", new Amount(1000, "EUR")
-                ))));
     }
 }
